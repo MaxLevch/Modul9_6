7d8ed1fbc2c7843c44f7b6cd4bb0461e72e769df
Макс Левченко <MaxiLeva@gmail.com>
2022-10-12 16:05:09 +0300

    Добавьте файлы проекта.

---
+++ Modul9_6/MetodReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9_6
+{
+    internal class MetodReader
+    {
+        string[] array;
+        public delegate void MetodDelegate(int metod, string[] array);
+        public event MetodDelegate MetodEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine("Введите 1 для сортировки по возрастанию, 2 для сортировки по убыванию");
+            int metod = Convert.ToInt32(Console.ReadLine());
+            if (metod != 1 && metod != 2) throw new Exception("Некорректное число");
+            MetodEntered(metod, array); 
+        }
+
+        protected virtual void MetodEntered(int metod, string[] array)
+        {
+            MetodEnteredEvent?.Invoke(metod, array);
+        }
+        public MetodReader(string[] arr)
+        {
+            array = arr;
+        }
+    }
+}
---
+++ Modul9_6/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Modul9_6
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string[] Surnames = WriteSurnames();
+            MetodReader metodReader = new MetodReader(Surnames);
+            metodReader.MetodEnteredEvent += SortSurname;
+            try
+            {
+                metodReader.Read();
+            }
+            // надеюсь правильно понял задание
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено некорректное значение");
+            }
+            catch (Exception ex) when (ex.Message == "Некорректное число")
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) when (ex is ArgumentNullException)
+            {
+                Console.WriteLine("Аргумент, передаваемый в метод — null");
+            }
+            catch (Exception ex) when (ex is ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Аргумент находится за пределами диапазона допустимых значений.");
+            }
+            catch (Exception ex) when (ex.Message == "Плохо с фантазией")
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Сортировка завершена");
+            foreach (string s in Surnames)
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+        }
+
+        static void SortSurname(int metod, string[] array)
+        {
+            switch (metod)
+            {
+                case 1:
+                    Array.Sort(array);
+                    break;
+                case 2:
+                    Array.Sort(array);
+                    Array.Reverse(array);
+                    break;
+            }
+        }
+
+        public static string[] WriteSurnames()
+        {
+            string[]surn = new string[5];
+            for (int i = 0; i < surn.Length; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} фамилию");
+                surn[i] = Console.ReadLine();
+            }
+            return surn;
+        }
+    }
+}
